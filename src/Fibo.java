b18167a7955e9628827b807312c21007698d9714
Melinda Marmot <melinda.marmot@epita.fr>
2022-02-10 15:29:50 +0100

    partie 6

---
+++ src/Fibo.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Fibo implements Command{
+    @Override
+    public String name() {
+        return "fibo";
+    }
+
+    @Override
+    public boolean run(Scanner scanner) {
+        System.out.println("Enter a number :");
+        System.out.println(fibo(scanner.nextInt()));
+        scanner.nextLine();
+
+        return false;
+    }
+
+    private static int fibo(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Should be positive");
+        }
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        return fibo(n-1) + fibo(n-2);
+    }
+}
--- src/Launcher.java
+++ src/Launcher.java
@@ -1,77 +1,33 @@
+import java.util.List;
 import java.util.Scanner;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.util.*;
-import java.util.stream.Collectors;
 
 public class Launcher {
-
-    private static int Fibo(int n) {
-        if (n < 0) {
-            throw new IllegalArgumentException("Should be positive");
-        }
-        if (n == 0 || n == 1) {
-            return n;
-        }
-        return Fibo(n-1) + Fibo(n-2);
-    }
-
-    private static void Freq(String filename) {
-        String content;
-        try {
-            content = java.nio.file.Files.readString(Path.of(filename));
-        } catch (IOException e) {
-            System.err.println("Unreadable file: " + e.getMessage());
-            return;
-        }
-
-        content = content.toLowerCase().replaceAll("[.!?\\-'\"\n]", " ");
-
-        Map<String, Integer> freq = new HashMap<>();
-
-        for (var word : content.split(" ")) {
-            if (word.isBlank()) continue;
-
-            freq.putIfAbsent(word, 0);
-            freq.put(word, freq.get(word) + 1);
-        }
-
-        List<String> words = new ArrayList<>();
-
-        while(words.size() < 3 && freq.keySet().size() > 0) {
-            int max = Collections.max(freq.values());
-            var keys = freq.keySet().stream().filter(k -> freq.get(k) == max).collect(Collectors.toList());
-            var last = keys.get(keys.size() - 1);
-            words.add(last);
-            freq.remove(last);
-        }
-
-        System.out.println(String.join(" ", words));
-    }
-
     public static void main(String[] args) {
-        System.out.println("Welcome to the Launcher Class!");
+        System.out.println("Welcome !");
+
+        List<Command> commands = List.of(
+                new Quit(),
+                new Fibo(),
+                new Freq()
+        );
+
         Scanner scanner = new Scanner(System.in);
-        boolean ShouldContinue = true;
+
         do {
             System.out.println("Enter some command:");
-            String entry = scanner.nextLine();
-            if (entry.equals("quit")) {
-                ShouldContinue = false;
+            String instruction = scanner.nextLine();
+            boolean shouldContinue = false;
+            for (var i : commands) {
+                if (i.name().equals(instruction)) {
+                    if (i.run(scanner))
+                        return;
+
+                    shouldContinue = true;
+                }
             }
-            else if (entry.equals("fibo")) {
-                System.out.print("Enter a number: ");
-                String num = scanner.nextLine();
-                int n = Integer.parseInt(num);
-                System.out.println(Fibo(n));
-            }
-            else if (entry.equals("freq")) {
-                System.out.println("Veuillez saisir le nom du fichier pour lequel vous voulez une analyse:");
-                Freq(scanner.nextLine());
-            }
-            else {
+
+            if (!shouldContinue)
                 System.out.println("Unknown command");
-            }
-        } while(ShouldContinue);
+        } while (true);
     }
 }
