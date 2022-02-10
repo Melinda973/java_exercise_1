78fd1300d1c3a1bcc7520c29da0b1f585a740d38
Melinda Marmot <melinda.marmot@epita.fr>
2022-02-10 15:05:38 +0100

    partie 4

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,13 +1,36 @@
-import java.util.List;
+import java.util.Scanner;
 
 public class Launcher {
+
+    private static int Fibo(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Should be positive");
+        }
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        return Fibo(n-1) + Fibo(n-2);
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to the Launcher Class!");
-        java.util.Scanner scanner = new java.util.Scanner(System.in);
-        String entry = scanner.nextLine();
-         while (!entry.equals("quit")) {
-            System.out.println("Unknown command");
-            entry = scanner.nextLine();
-        }
+        Scanner scanner = new Scanner(System.in);
+        boolean ShouldContinue = true;
+        do {
+            System.out.println("Enter some command:");
+            String entry = scanner.nextLine();
+            if (entry.equals("quit")) {
+                ShouldContinue = false;
+            }
+            else if (entry.equals("fibo")) {
+                System.out.print("Enter a number: ");
+                String num = scanner.nextLine();
+                int n = Integer.parseInt(num);
+                System.out.println(Fibo(n));
+            }
+            else {
+                System.out.println("Unknown command");
+            }
+        } while(ShouldContinue);
     }
 }
